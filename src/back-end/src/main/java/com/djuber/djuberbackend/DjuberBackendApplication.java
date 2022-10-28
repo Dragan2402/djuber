10b4cc3b2676c9b61fa4f6ee12b027707dce8ca7
Dragan2402 <draganmirkovic2402@gmail.com>
2022-10-28 17:34:39 +0200

    Backend project initialize

---
+++ src/back-end/src/main/java/com/djuber/djuberbackend/DjuberBackendApplication.java
@@ -0,0 +1,13 @@
+package com.djuber.djuberbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjuberBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjuberBackendApplication.class, args);
+    }
+
+}
---
+++ src/back-end/src/test/java/com/djuber/djuberbackend/DjuberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djuber.djuberbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjuberBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
